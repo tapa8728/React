1268776605b93d881babbed94e902b23d591156c
Tanvi parikh <tapa8728@colorado.edu>
2018-06-12 14:50:25 -0700

    js or jsx

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+// when this file is imported in  main file index.js (main because thats whats linked to index.html), 
+// only the "export" entities will be imported. In this case, only the component App will be exported
+
+// this file can be saved as .js or .jsx 
+
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
