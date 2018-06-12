673f12b422aa0e010328d2c73eec4cd8b1c8f478
Tanvi parikh <tapa8728@colorado.edu>
2018-06-12 15:30:43 -0700

    Created my first React component

--- src/App.js
+++ src/App.js
@@ -5,19 +5,27 @@
 
 // the embedded html code is compiled to .jsx
 
+// hence we dont prefer the createElement as it get cumbersome when we add more children elements. 
+// the embedded HTML is actually JS.  
+
+// while importing another component, 
+
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
-    // return (
-    //   <div className="App">
-    //     <h1> I am react app </h1>
-    //   </div>
-    // );
+    return (
+      <div className="App">
+        <h1> I am react app </h1>
+        <p> this is really working</p>
+        <Person />
+      </div>
+    );
 
-    return React.createElement('div', {className:"App"}, React.createElement('h1', null,'I am react app'));
+    //return React.createElement('div', {className:"App"}, React.createElement('h1', null,'I am react app'));
   }
 }
 
---
+++ src/Person/Person.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Person = () => {
+	return (
+		<p> I am a person </p>
+	);
+}
+
+export default Person
