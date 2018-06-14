158a221ebb9244f579536641bea7527c4d998c97
Tanvi parikh <tapa8728@colorado.edu>
2018-06-13 17:45:32 -0700

    Inline CSS styling for specific element added

--- src/App.js
+++ src/App.js
@@ -66,11 +66,25 @@ class App extends Component {
   }
 
   render() {
+
+    // inline button styling
+    const buttonStyle = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
     return (
       <div className="App">
         <h1> I am react app </h1>
         <p> this is really working</p>
-        <button onClick={this.switchEventHandler.bind(this, 'Tipsy', 'Pie')}> Switch Name </button>
+        <button 
+          style={buttonStyle}
+          onClick={this.switchEventHandler.bind(this, 'Tipsy', 'Pie')}
+        > Switch Name </button>
+
         <Person 
           name={this.state.persons[0].name} 
           age={this.state.persons[0].age} 
