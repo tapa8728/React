9771be68ea1d357abb9b29d53a96ae748e604191
Tanvi parikh <tapa8728@colorado.edu>
2018-06-13 17:40:14 -0700

    Added Person (component) specific CSS

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,9 +3,11 @@
 // Adding dynamic content to the component
 import React from 'react';
 
+import './Person.css'
+
 const Person = (props) => {
 	return (
-		<div>
+		<div className="Person">
 			<p onClick={props.click}> I am {props.name} and I am {props.age} years old. </p>
 			<p> {props.children} </p>
 			<input type="text" onChange={props.changed}  value={props.name}/>
