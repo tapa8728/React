009ad8844460aeecd155cbedd88563e0deb6272f
Tanvi parikh <tapa8728@colorado.edu>
2018-06-24 16:25:15 -0700

    Throw an error deliberately and then hopefully handle it

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -7,9 +7,15 @@ import React from 'react';
 import classes from './Person.css'
 
 const Person = (props) => {
+		// if you want to throw and error deliberately. 
+		// const rnd = Math.random();
 
+		// if (rnd >0.7) {
+		// 	throw new Error('Random hit error');
+		// }
 
 	return (
+
 		
 		<div className="Person" >
 			<p onClick={props.click}> I am {props.name} and I am {props.age} years old. </p>
