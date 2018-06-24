6bf38e832cce1673cadd0718e46614e42751121f
Tanvi parikh <tapa8728@colorado.edu>
2018-06-24 16:32:44 -0700

    Error  Boundary component created

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -15,8 +15,6 @@ const Person = (props) => {
 		// }
 
 	return (
-
-		
 		<div className="Person" >
 			<p onClick={props.click}> I am {props.name} and I am {props.age} years old. </p>
 			<p> {props.children} </p>
