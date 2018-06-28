3bf8108b060715e90137848c97eb73fd29f4c6bd
Tanvi parikh <tapa8728@colorado.edu>
2018-06-27 21:34:05 -0700

    lifecyecle hooks added to Person

--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -7,8 +7,24 @@ import React, { Component } from 'react';
 import classes from './Person.css'
 
 class Person extends Component{
+	// adding all the lifecycle hooks
+	constructor(props){
+	    super(props); //you have to add super 
+	    console.log("[Person.js] Inside Constructor ", props);
+	}
+
+	// gets executed after constructor. 
+	componentWillMount(){
+		console.log("[Person.js] Inside componentWillMount ");
+	}
+
+	// gets executed after render. 
+	componentDidMount(){
+		console.log("[Person.js] Inside componentDidMount ");
+	}
 
 	render() {
+		console.log("[Person.js] Inside render ");
 		return (
 			<div className="Person" >
 				<p onClick={this.props.click}> I am {this.props.name} and I am {this.props.age} years old. </p>
