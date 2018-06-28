12a2fa05d487da6405aafda20c59f2f826afaca8
Tanvi parikh <tapa8728@colorado.edu>
2018-06-28 01:07:19 -0700

    we done

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -4,7 +4,7 @@ import Aux from '../../hoc/Aux'
 const cockpit = (props) => {
 
 	return (
-		<div>
+		<Aux>
 			<h1>{props.title}</h1>
 			<h2> I am react app </h2>
 			<p className={props.ac.join(' ')}> this is really working</p>
@@ -12,7 +12,7 @@ const cockpit = (props) => {
 				style={props.btnStyle}
 				onClick={props.clicked}>
 				Toggle Name </button>
-		</div>
+		</Aux>
 	);
 
 };
