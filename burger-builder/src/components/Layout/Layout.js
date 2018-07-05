23a4758b2ea859645ab5215406370287b3090baa
Tanvi parikh <tapa8728@colorado.edu>
2018-07-05 13:57:07 -0700

    layout, aux component ready to be filled out.

--- burger-builder/src/components/Layout/Layout.js
+++ burger-builder/src/components/Layout/Layout.js
@@ -1,1 +1,21 @@
 // Component right below the root component 
+
+/*
+Aux: wrapped in a high-order Aux component
+*/
+
+import React from 'react';
+import Aux from '../../hoc/Aux';
+
+
+const layout = (props) => (
+	<Aux>
+		<div>Toolbar, Sidebar, Backdrop</div>
+		<main>
+			{props.children}
+		</main> 
+	</Aux>
+
+);
+
+export default layout;
---
+++ burger-builder/src/hoc/Aux.js
@@ -0,0 +1,7 @@
+// higher order auxillary stateless compoenent
+
+import React from 'react';
+
+const aux = (props) => props.children;
+
+export default aux;
