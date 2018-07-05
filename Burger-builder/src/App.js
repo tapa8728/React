1504de102f2296f38083169dd64e33e758e1d28f
Tanvi parikh <tapa8728@colorado.edu>
2018-07-05 15:29:57 -0700

    bb component

--- Burger-builder/src/App.js
+++ Burger-builder/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Layout from './components/Layout/Layout'
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 
 class App extends Component {
   render() {
     return (
       <Layout>
         <p> Test Component: but in actuality a page we actually want to display </p>
+        <BurgerBuilder />
       </Layout>
     );
   }
--- burger-builder/src/containers/BurgerBuilder/burgerbuilder.js
+++ burger-builder/src/containers/BurgerBuilder/burgerbuilder.js
@@ -6,6 +6,7 @@ import Aux from '../../hoc/Aux';
 class BurgerBuilder extends Component{
 	render(){
 		<Aux>
+			<p> hello BurgerBuilder! </p>
 			<div> Burger Graphical representation </div>
 			<div> Build Controls </div>
 		</Aux>
