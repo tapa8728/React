3e57b04c6080c72eff335746b7e86f7463818e79
Tanvi parikh <tapa8728@colorado.edu>
2018-07-05 15:28:27 -0700

    bb component

--- burger-builder/src/containers/BurgerBuilder/burgerbuilder.js
+++ burger-builder/src/containers/BurgerBuilder/burgerbuilder.js
@@ -1,1 +1,15 @@
 // Main stateful component that will maintain the state of this application
+
+import React, {Component} from 'react';
+import Aux from '../../hoc/Aux';
+
+class BurgerBuilder extends Component{
+	render(){
+		<Aux>
+			<div> Burger Graphical representation </div>
+			<div> Build Controls </div>
+		</Aux>
+	}
+}
+
+export default BurgerBuilder;
