f9901c22446801b936c3ccff0a0dacf0a50880c2
Tanvi parikh <tapa8728@colorado.edu>
2018-07-05 17:16:14 -0700

    We are ready to build a burger

--- burger-builder/src/components/Burger/Burger.js
+++ burger-builder/src/components/Burger/Burger.js
@@ -1,1 +1,13 @@
 // This is the burger that we are actually rendering to the screen
+
+import React from 'react';
+import Aux from '../../hoc/Aux';
+
+const burger = (props) => (
+	<Aux>
+		// wrap the burger ingredients components here. 
+
+	</Aux>
+	);
+
+export default burger;
--- burger-builder/src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ burger-builder/src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,1 +1,14 @@
 // represents a specific burger ingredient
+
+import React from 'react';
+import Aux from '../../hoc/Aux';
+import './BurgerIngredient.css';
+
+const burgerIngredient = (props) => (
+		<Aux>
+			<p className = {BreadBottom}> I am a BreadBottom burgerIngredient</p>
+		</Aux>
+
+	);
+
+export default burgerIngredient;
