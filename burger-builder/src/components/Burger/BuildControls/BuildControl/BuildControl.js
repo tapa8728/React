9b4d8f5797e3aa5ab3a30ce8abc5532443a61f59
Tanvi parikh <tapa8728@colorado.edu>
2018-07-07 18:40:09 -0700

    More ingredients can be added

--- burger-builder/src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ burger-builder/src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -5,8 +5,8 @@ import './BuildControl.css';
 const buildControl = (props) => (
 	<div className="BuildControl">
 		<div className="Label">{props.label}</div>
-		<button className="Less"> Less </button>
-		<button className="More"> More </button>
+		<button className="Less" > Less </button>
+		<button className="More" onClick={props.added}> More </button>
 	</div>
 );
 
--- burger-builder/src/components/Burger/BuildControls/BuildControls.js
+++ burger-builder/src/components/Burger/BuildControls/BuildControls.js
@@ -19,7 +19,7 @@ const buildControls = (props) => (
 			<BuildControl 
 				key={ctrl.label} 
 				label={ctrl.label} 
-				added={props.ingredientAdded}
+				added= {()=>props.ingredientAdded(ctrl.type)}
 				removed={props.ingredientRemoved}
 			/>
 
